f983bb0e0f7af4d6dd9993afe293cd2f406ad521
Larissa Novais <larinovais54@gmail.com>
2021-09-02 11:34:52 -0300

    terminando exercicio5

--- src/exercicioDeRevisão/Exercicio5.java
+++ src/exercicioDeRevisão/Exercicio5.java
@@ -1,8 +1,28 @@
 package exercicioDeRevisão;
 //Faça um programa que leia 5 números e informe o maior número.
 
-public class Exercicio5 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Exercicio5 {
+
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int contador = 1;
+        double maiorValor = 0;
+
+        System.out.println("Escolha 5 numeros para saber qual é o maior");
+
+        while (contador <= 5){
+            System.out.println("Digite o "+contador+ " :");
+            double numeroDoUsuario = leitor.nextDouble();
+            contador++;
+
+            if (numeroDoUsuario > maiorValor){
+                maiorValor = numeroDoUsuario;
+            }
+        }
+        System.out.println("Maior valor é igual a " +maiorValor);
     }
 }
